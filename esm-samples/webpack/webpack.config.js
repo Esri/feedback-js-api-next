cacbfebd5682990f0affa7dac0f551185d23631f
Rene Rubalcava <rrubalcava@esri.com>
2020-10-29 14:53:18 -0700

    update for esm and next webpack-plugin

--- esm-samples/webpack/webpack.config.js
+++ esm-samples/webpack/webpack.config.js
@@ -1,40 +1,50 @@
-const path = require("path");
+const path = require('path');
 
-const CopyPlugin = require("copy-webpack-plugin");
-const HtmlWebPackPlugin = require("html-webpack-plugin");
-const MomentLocalesPlugin = require("moment-locales-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const ArcGISPlugin = require('@arcgis/webpack-plugin');
 
 module.exports = {
   entry: {
-    index: "./src/index.js"
+    index: ['./src/index.css', './src/index.js']
   },
   node: false,
   output: {
-    filename: "main.js",
-    chunkFilename: "chunks/[id].js",
-    publicPath: ""
+    path: path.join(__dirname, 'dist'),
+    chunkFilename: 'chunks/[id].js',
+    publicPath: ''
   },
   devServer: {
-    contentBase: path.join(__dirname, "dist"),
+    contentBase: path.join(__dirname, 'dist'),
     compress: true,
     port: 3001,
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader'
+        ]
+      },
+    ]
+  },
   plugins: [
-    new CopyPlugin({
-      patterns: [
-        {
-          from: "node_modules/@arcgis/core/assets",
-          to: "assets"
-        }
-      ]
-    }),
+    new CleanWebpackPlugin(),
+    new ArcGISPlugin({ locales: ['en'] }),
     new HtmlWebPackPlugin({
-      title: "ArcGIS API  for JavaScript",
-      template: "./public/index.html",
-      filename: "./index.html",
-      chunksSortMode: "none",
-      inlineSource: ".(css)$"
+      title: 'ArcGIS API  for JavaScript',
+      template: './public/index.html',
+      filename: './index.html',
+      chunksSortMode: 'none',
+      inlineSource: '.(css)$'
     }),
-    new MomentLocalesPlugin()
+    new MiniCssExtractPlugin({
+      filename: "[name].[chunkhash].css",
+      chunkFilename: "[id].css"
+    })
   ]
 };
