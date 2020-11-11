dceee87bd1eef301f0284be16ce1bd2f27152383
Rene Rubalcava <rrubalcava@esri.com>
2020-11-11 10:45:10 -0800

    update for latest next release

--- esm-samples/jsapi-node/src/projection.js
+++ esm-samples/jsapi-node/src/projection.js
@@ -2,7 +2,7 @@ import esriConfig from "@arcgis/core/config.js";
 import * as pe from "@arcgis/core/geometry/projection.js";
 import Point from "@arcgis/core/geometry/Point.js";
 
-esriConfig.baseUrl = "node_modules/@arcgis/core/"; // relative to when running in root
+esriConfig.assetsPath = "node_modules/@arcgis/core/assets"; // relative to when running in root
 
 pe.load()
   .then(() => {
